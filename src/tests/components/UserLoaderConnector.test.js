99c9ccd3c2647c9ad9fd51c8df5efe6de4767ab1
Andrew <andrew.schubert1986@gmail.com>
2019-03-16 01:48:18 -0500

    chore: test

--- src/tests/components/UserLoaderConnector.test.js
+++ src/tests/components/UserLoaderConnector.test.js
@@ -13,40 +13,31 @@ import UserLoaderConnector from '../../components/UserLoaderConnector';
 jest.mock('axios');
 
 describe('Given UserLoaderConnector', () => {
-  it('test', () => {
-    expect(true).toBe(true);
+  const props = {
+    children: mockComponent()
+  };
+
+  describe('and its attempting to load a user', () => {
+    it('should load a user', async () => {
+      const axiosResponse = mockAxiosResponse(mockUser());
+
+      axios.get.mockResolvedValue(axiosResponse);
+
+      const { store } = renderWithRedux(<UserLoaderConnector {...props} />);
+
+      expect(store.getState().app.loadCounter.count).toEqual(1);
+
+      await wait(() => {
+        expect(store.getState().user).toEqual(axiosResponse.data);
+        expect(store.getState().app.loadCounter.count).toEqual(0);
+      });
+    });
   });
-  // console.log('start of UserLoaderConnector');
-  // const props = {
-  //   children: mockComponent()
-  // };
 
-  // describe('and its attempting to load a user', () => {
-  //   it('should load a user', async () => {
-  //     console.log(mockAxiosResponse(mockUser()));
-
-  //     const axiosResponse = mockAxiosResponse(mockUser());
-
-  //     axios.get.mockResolvedValue(axiosResponse);
-
-  //     const { store } = renderWithRedux(<UserLoaderConnector {...props} />);
-
-  //     expect(store.getState().app.loadCounter.count).toEqual(1);
-
-  //     await wait(() => {
-  //       console.log('i am waiting in the async call');
-  //       expect(store.getState().user).toEqual(axiosResponse.data);
-  //       expect(store.getState().app.loadCounter.count).toEqual(0);
-  //       console.log('i am at the end of the async');
-  //     });
-  //   });
-  // });
-
-  // it('should render its children', () => {
-  //   console.log('render the children');
-  //   const { queryByTestId } = renderWithRedux(
-  //     <UserLoaderConnector {...props} />
-  //   );
-  //   expect(queryByTestId('mock-component')).toBeInTheDocument();
-  // });
+  it('should render its children', () => {
+    const { queryByTestId } = renderWithRedux(
+      <UserLoaderConnector {...props} />
+    );
+    expect(queryByTestId('mock-component')).toBeInTheDocument();
+  });
 });
